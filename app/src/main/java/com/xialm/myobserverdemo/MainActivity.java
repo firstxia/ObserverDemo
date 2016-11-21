fb0bca2e3d57c4c1ed45bc9637c2968164b9d47f
Xialm <firstxia168@163.com>
2016-11-21 22:35:40 +0800

    观察者模式完成--继承自系统的Observable

--- app/src/main/java/com/xialm/myobserverdemo/MainActivity.java
+++ app/src/main/java/com/xialm/myobserverdemo/MainActivity.java
@@ -2,15 +2,28 @@ package com.xialm.myobserverdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * 观察者模式:小Demo
  */
 public class MainActivity extends AppCompatActivity {
 
+    private DownloadManager downloadManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 创建观察者对象
+        downloadManager = DownloadManager.getInstance();
+        // 注册观察者
+        downloadManager.addObserver(new ItemView());
+        downloadManager.addObserver(new DetailView());
+    }
+
+    public void notifyAll(View view) {
+        // 通知所有的观察者
+        downloadManager.notifyObservers();
     }
 }
