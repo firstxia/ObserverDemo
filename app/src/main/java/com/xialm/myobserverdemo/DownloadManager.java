95f5267b78954d3efd2329f2053340b0cc859e0c
Xialm <firstxia168@163.com>
2016-11-21 23:06:47 +0800

    观察者模式完成--使用自定义的Observer(无参数版)

--- app/src/main/java/com/xialm/myobserverdemo/DownloadManager.java
+++ app/src/main/java/com/xialm/myobserverdemo/DownloadManager.java
@@ -1,22 +1,46 @@
 package com.xialm.myobserverdemo;
 
-import java.util.Observable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * 被观察者
+ * 自定义的被观察者--使用单例
  * Created by Xialm on 2016/11/21.
  */
 
-public class DownloadManager extends Observable {
+public class DownloadManager {
     private static DownloadManager instance = new DownloadManager();
-    private DownloadManager() {}
+
+    private DownloadManager() {
+    }
 
     public static DownloadManager getInstance() {
         return instance;
     }
 
-    @Override
-    public synchronized boolean hasChanged() {
-        return true;
+    // 2.声明集合-存放所有的观察者
+    private List<DownloadObserver> observers = new ArrayList<>();
+
+    // 1.声明一个观察者-接口
+    public interface DownloadObserver {
+        void update();
+    }
+
+    // 3.添加一个注册观察者的方法
+    public void addObserver(DownloadObserver observer) {
+        if (observer == null) {
+            throw new NullPointerException("你给我传的观察者不能为空");
+        }
+        if (!observers.contains(observer)) {
+            observers.add(observer);
+        }
+    }
+
+    // 4.添加一个通知所有观察者的方法
+    public void notifyObservers() {
+        for (int i = 0; i < observers.size(); i++) {
+            DownloadObserver downloadObserver = observers.get(i);
+            downloadObserver.update();
+        }
     }
 }
--- app/src/main/java/com/xialm/myobserverdemo/ItemView.java
+++ app/src/main/java/com/xialm/myobserverdemo/ItemView.java
@@ -1,16 +1,14 @@
 package com.xialm.myobserverdemo;
 
-import java.util.Observable;
-import java.util.Observer;
-
 /**
  * 观察者1
  * Created by Xialm on 2016/11/21.
  */
 
-public class ItemView implements Observer {
+public class ItemView implements DownloadManager.DownloadObserver {
+
     @Override
-    public void update(Observable observable, Object o) {
-        System.out.println("观察者1--ItemView:收到了消息");
+    public void update() {
+        System.out.println("观察者1--ItemView:收到了消息--自定义");
     }
 }
